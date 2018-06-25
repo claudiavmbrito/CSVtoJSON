1c3adb6995056dd1f1bd039c0b6b51ea28dcddc4
Cláudia Brito <cvmb.mat@gmail.com>
2018-06-25 11:21:31 +0100

    small changes

--- csvtojson.py
+++ csvtojson.py
@@ -3,13 +3,13 @@ import sys
 import json
 
 
-fieldnames=["timestamp","channel 1","channel 2"]
+fields=["timestamp","channel 1","channel 2"]
 
 def convert(filename):
   csv_filename = filename[0]
   print("Opening CSV file: " + csv_filename)
   with open(csv_filename, 'r') as f:
-    csv_reader = csv.DictReader(f,fieldnames)
+    csv_reader = csv.DictReader(f,fields)
     json_filename = csv_filename.split(".")[0]+".json"
     print("Saving JSON to file: " + json_filename)
     with open(json_filename,'w') as jsonf:
